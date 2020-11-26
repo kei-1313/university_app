957717c908be8e18e11864988e6d0cfd77ae8016
kei-1313 <candydog666@gmail.com>
2020-11-27 02:37:43 +0900

    デプロイ準備

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << "kyusan-university-app.herokuapp.com"
+  config.web_console.whitelisted_ips = '172.20.0.1'
 end

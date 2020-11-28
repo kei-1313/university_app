1cf4a6b110bd50d638db31b2aedf09802feaff03
kei-1313 <candydog666@gmail.com>
2020-11-28 12:31:08 +0900

    DM一覧追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,7 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @rooms = current_user.rooms.includes(:messages).order("messages.created_at desc")
+    @rooms = current_user.rooms
   end
 
   def create 

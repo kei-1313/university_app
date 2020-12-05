04d2c5feda1e5f90c9b8152c1a707e05608f0362
kei-1313 <candydog666@gmail.com>
2020-12-05 22:11:04 +0900

    redirect_backを削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,12 @@ class LikesController < ApplicationController
     #通知機能追加
     @post = Post.find(params[:post_id])  #どの投稿にいいねしたか取得
     @post.create_notification_like!(current_user)
-    redirect_back(fallback_location: root_path) #直前のページに飛ばしている（エラーが出るとroot_pathに飛ぶようになっている）
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
   end
+
 end

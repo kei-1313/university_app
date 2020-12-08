7078a52c837bc6c7bcbfe0d68475329000bcee9e
kei-1313 <candydog666@gmail.com>
2020-12-08 12:33:52 +0900

    テスト変更

--- spec/features/notifications_spec.rb
+++ spec/features/notifications_spec.rb
@@ -106,34 +106,6 @@ RSpec.feature "Notifications", type: :feature do
     expect(page).to have_content "#{other_user.username}さんが#{other_user.username}さんの投稿にコメントしました"
   end
 
-  # scenario "いいねされた場合の通知" do
-  #   user = FactoryBot.create(:user, :with_posts)
-  #   other_user = FactoryBot.create(:user)
-  #   sign_in_as other_user
-
-  #   within ".header-menu" do
-  #     click_link "投稿一覧"
-  #   end
-
-  #   first(".link_to_allpost").click
-
-  #   find(".like_btn").click
-
-  #   within ".header-menu" do
-  #     click_link "ログアウト"
-  #   end
-  #   expect(page).to have_content "ログアウトしました。"
-    
-  #   sign_in_as user
-
-  #   within ".header-menu" do
-  #     click_link "通知"
-  #   end
-
-  #   expect(page).to have_content "通知"
-  #   expect(page).to have_content "#{other_user.username}さんがあなたの投稿にいいねしました"
-  # end
-
   scenario "通知を全件削除" do
     user = FactoryBot.create(:user, :with_posts)
     other_user = FactoryBot.create(:user)
